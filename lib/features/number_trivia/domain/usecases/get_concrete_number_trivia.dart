8441b165a178ac86d9a70d5f01b6a89ca32f1c7c
tsunoda-m-e-ize <m.tsunoda@e-ize.jp>
2022-05-16 09:12:03 +0900

    initial commit

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:tddcleanarchitecture/core/failures.dart';
+import 'package:tddcleanarchitecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tddcleanarchitecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
