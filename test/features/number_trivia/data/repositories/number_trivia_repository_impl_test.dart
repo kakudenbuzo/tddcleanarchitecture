82f9231e676146519df3f2b7f60f2e51a1a155c8
kakudenbuzo <kakudenbuzou@gmail.com>
2022-06-01 08:37:48 +0900

    chapter5

---
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:tddcleanarchitecture/core/platform/network_info.dart';
+import 'package:tddcleanarchitecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:tddcleanarchitecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:tddcleanarchitecture/features/number_trivia/data/repositories/number_trivia_repositories_impl.dart';
+
+import 'number_trivia_repository_impl_test.mocks.dart';
+
+@GenerateMocks(
+    [NumberTriviaRemoteDataSource, NumberTriviaLocalDataSource, NetworkInfo])
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  MockNumberTriviaRemoteDataSource mockRemoteDataSource;
+  MockNumberTriviaLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockNumberTriviaRemoteDataSource();
+    mockLocalDataSource = MockNumberTriviaLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repository = NumberTriviaRepositoryImpl(
+        remoteDataSource: mockRemoteDataSource,
+        localDataSource: mockLocalDataSource,
+        networkInfo: mockNetworkInfo);
+  });
+}
