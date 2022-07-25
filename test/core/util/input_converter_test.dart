7040862aa9d29e8a9f801533c8fec4be8b49737a
kakudenbuzo <kakudenbuzou@gmail.com>
2022-07-25 19:36:31 +0900

    chapter 10

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tddcleanarchitecture/core/util/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test("should return integer when the string represents an unsigned integer",
+        () {
+      // arrange
+      const str = '123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, const Right(123));
+    });
+
+    test("should return a Failure when the string is not an integer", () {
+      // arrange
+      const str = 'abc';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+
+    test("should return a Failure when the string is a  nagetive integer", () {
+      // arrange
+      const str = '-123';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
